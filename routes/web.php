67ff08a46e6364b1fc65e1cab0b8c6e95d635e7c
Marco <phpjunky87@gmail.com>
2023-12-10 19:41:19 +0100

    main
    Update Route file

--- routes/web.php
+++ routes/web.php
@@ -13,29 +13,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get(
-    '/',
-    [NumberConverterController::class, 'index']
-)->name('index');
-Route::get(
-    '/result',
-    [NumberConverterController::class, 'result']
-)->name('result');
+Route::controller(NumberConverterController::class)->group(function () {
+    Route::get('/','index')
+        ->name('index');
+    Route::get('/result', 'result')
+        ->name('result');
+    Route::post('/convert','convert')
+        ->name('convert');
 
-Route::post(
-    '/convert',
-    [NumberConverterController::class, 'convert']
-)->name('convert');
+});
 
-Route::get('/{locale?}', function ($locale = null) {
+Route::get('/{locale?}', function (?string $locale = null) {
     if (isset($locale) && in_array($locale, config('locales.locales'))) {
         app()->setLocale($locale);
     }
     return view('/');
-});
+})->name('locale');
 
-Route::get('language/{locale}', function ($locale) {
+Route::get('language/{locale}', function (string $locale) {
     app()->setLocale($locale);
     session()->put('locale', $locale);
     return redirect()->back();
-});
+})->name('language');
